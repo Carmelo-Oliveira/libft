da0601b21441184e69c96306da809c05ece7c38b
Carmelo Oliveira De Brito Lira Filho <carmoliv@c1r12s7.42porto.com>
2025-04-13 18:25:43 +0100

    Update

--- libft.h
+++ libft.h
@@ -8,14 +8,17 @@ int     ft_isdigit(int c);
 int     ft_isalnum(int c);
 int     ft_isascii(int c);
 int     ft_isprint(int c);
-size_t  ft_strlen(const char *str);
+size_t	ft_strlen(const char *str);
 void    *ft_memset(void *ptr, int value, size_t num);
-void    *ft_memcpy(void *dest, const void *src, size_t n);
-void    *ft_memove(void *dest, const void *src, size_t n);
-size_t   ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 int     ft_toupper(int c);
 int     ft_tolower(int c);
-void	bzero(void *s, size_t len);
+void	ft_bzero(void *s, size_t len);
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
 
 
 
